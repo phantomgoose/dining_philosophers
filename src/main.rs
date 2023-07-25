130bba2c92bdf121fa4b88386e64d6e0a7ed503a
phantomgoose <alex.txt@gmail.com>
2023-07-25 15:11:07 -0700

    cleanup/comments

--- src/main.rs
+++ src/main.rs
@@ -29,14 +29,21 @@ impl<'a> Philosopher<'a> {
         let right_fork = self.right_fork.try_lock();
 
         if let (Ok(_), Ok(_)) = (left_fork, right_fork) {
+            // Only proceed with eating if we were able to acquire both fork locks
             println!("{} is eating...", &self.name);
             thread::sleep(Duration::from_millis(10));
+
+            // Mutex locks on the forks will be released after this closure,
+            // because that's how long the `_` placeholder vars live that hold a reference to these locks.
             return;
         }
 
-        // forks aren't available, need to wait
+        // Both forks aren't yet available, need to wait.
+        // An alternative to waiting might be to break the fork symmetry such that one of the
+        // philosophers locks the right fork before the left, thus preventing deadlocks.
+        // I find that alternative solution less elegant though.
         thread::sleep(Duration::from_millis(5));
-        // println!("{} is waiting for forks before eating...", &self.name);
+        println!("{} is waiting for forks before eating...", &self.name);
         self.eat();
     }
 }
@@ -45,11 +52,9 @@ static PHILOSOPHERS: &[&str] = &["Socrates", "Plato", "Aristotle", "Thales", "Py
 
 fn main() {
     // Create forks
-    let mut forks: Vec<ThreadSafeFork> = Vec::with_capacity(5);
-
-    for _ in 0..5 {
-        forks.push(Arc::new(Mutex::new(Fork)));
-    }
+    let forks: Vec<ThreadSafeFork> = (0..PHILOSOPHERS.len())
+        .map(|_| Arc::new(Mutex::new(Fork)))
+        .collect();
 
     // Create philosophers
     let (thoughts_tx, thoughts_rx) = mpsc::channel();
@@ -57,13 +62,14 @@ fn main() {
     // Make each of them think and eat 100 times
     for (idx, philosopher_name) in PHILOSOPHERS.iter().enumerate() {
         let philosopher_thoughts_tx = thoughts_tx.clone();
+
         let left_fork = forks
             .get((idx + forks.len() - 1) % forks.len())
-            .unwrap()
+            .expect("Left fork should exist")
             .clone();
         let right_fork = forks
             .get((idx + forks.len() + 1) % forks.len())
-            .unwrap()
+            .expect("Right fork should exist")
             .clone();
 
         thread::spawn(move || {
@@ -80,12 +86,12 @@ fn main() {
         });
     }
 
-    // drop the main channel transmitter, so that the channel closes once all child threads finish
+    // Drop the main channel transmitter, so that the channel closes once all child threads finish
     drop(thoughts_tx);
 
     // Output their thoughts
     for thought in thoughts_rx {
-        println!("{}", thought);
+        println!("{thought}");
     }
 
     println!("done!");
